d9cb975da31368eaa80e708ea33c026c523b9b36
Windisley <windisleyl@gmail.com>
2024-11-21 13:27:48 -0300

    Projeto Mega Cena Pronto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 "use strict";
 const $inputs = document.querySelectorAll("[data-number]");
 const $button = document.querySelector("[data-button]");
+const $inputgame = document.querySelector("[data-game]");
 $button?.addEventListener("click", () => {
+    let Start = false;
+    let Empty = false;
     $inputs.forEach((input) => {
-        if (input) {
-            input.value = "10";
+        if ($inputgame.value != "") {
+            const randonnumber = Math.random() * 50;
+            const parcedstring = randonnumber.toFixed(2);
+            input.value = parcedstring;
+            if (input.value === $inputgame.value) {
+                Start = true;
+            }
+        }
+        else {
+            Empty = true;
         }
     });
-    console.log("oi");
+    if (Empty) {
+        alert("Informe os Numeros para Jogar");
+    }
+    if (Start) {
+        alert("Parabens Você Agora E Um Milionario!!!");
+    }
+    else {
+        alert("Não Foi Dessa Vez Que Você Ficou Milionário!");
+    }
 });
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,15 +1,42 @@
 
 const $inputs = document.querySelectorAll("[data-number]") as NodeListOf <HTMLInputElement>
-const $button = document.querySelector("[data-button]")
+const $button = document.querySelector("[data-button]") as HTMLButtonElement | null
+const $inputgame = document.querySelector("[data-game]") as  HTMLInputElement
 
 
 $button?.addEventListener("click", ()=>{
+    let Start:boolean = false
+    let Empty:boolean = false
     $inputs.forEach((input)=>{
-       if(input){
-           input.value = "10"
-       }
+      
+        if($inputgame.value != ""){
+            const randonnumber:number = Math.random() * 50
+            const parcedstring:string = randonnumber.toFixed(2)
+     
+            input.value = parcedstring
+            
+            if(input.value === $inputgame.value){
+                Start = true
+            }
+        }else{
+         Empty  = true
+    
+        }
+        
+        
+        
     })
 
-    console.log("oi")
+    if(Empty){
+        alert("Informe os Numeros para Jogar")  
+    }
+
+    if(Start){
+        alert("Parabens Você Agora E Um Milionario!!!")
+    }else{
+        alert("Não Foi Dessa Vez Que Você Ficou Milionário!")
+    }
+    
+   
 
 })
